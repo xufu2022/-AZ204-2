4fa2a52057c335990f985bf3878e95d74b7ed5db
Your Name <you@example.com>
2022-06-23 15:01:04 +0100

    ok

---
+++ AzureFunApp/AzureFunc1/AzureFunc1/GetProduct.cs
@@ -0,0 +1,24 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace AzureFunc1
+{
+    public static class GetProduct
+    {
+        [FunctionName("GetProduct")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            var obj = new { id=5, name="dfjie"};
+            return   new OkObjectResult(obj);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# -AZ204-2
+# Az Function
+
+
+Create a resource ==> Function App
+
+# Az Powershell and ARM
+
+Installation: 
+    set execution policy
+    install module Az (allow cobbler...)
+
